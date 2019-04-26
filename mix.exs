1b9d983a83b4f16d3354334aaecd0c3577dabc09
pmargreff <pmargreff@gmail.com>
2019-04-25 21:49:48 -0300

    Add credo on dependencies

--- mix.exs
+++ mix.exs
@@ -11,16 +11,16 @@ defmodule PaintEasy.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

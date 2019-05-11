d3781426ba673ed7f5be06c91a72e6620c443c6b
pmargreff <pmargreff@gmail.com>
2019-05-11 16:55:34 -0300

    refactor(edit): rewrite sintaxe with lambda version

--- test/paint_easy/editor_test.exs
+++ test/paint_easy/editor_test.exs
@@ -26,25 +26,15 @@ defmodule PaintEasy.EditorTest do
     test "create an line on image" do
       {:ok, image} = Creator.new_pbm(width: 3, height: 3)
 
-      %{pixels: pixels} = Editor.edit(image, &TestFilters.line/1)
+      fun_filter = fn {pixel, index} ->
+        case index do
+          idx when idx >= 3 and idx <= 5 -> 1
+          _ -> pixel
+        end
+      end
+
+      %{pixels: pixels} = Editor.edit(image, fun_filter)
       assert pixels == [0, 0, 0, 1, 1, 1, 0, 0, 0]
     end
-
-    test "create a vertical line on image" do
-    end
-
-    test "create a horizontal line on image" do
-    end
-
-    test "create a square on image" do
-    end
   end
-end
-
-defmodule TestFilters do
-  @moduledoc """
-  Module with simple functions to test image editor
-  """
-  def line({_pixel, index}) when index >= 3 and index <= 5, do: 1
-  def line({pixel, _index}), do: pixel
 end

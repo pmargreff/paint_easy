3e15ad7d6e46e3203f901e83cd78595070aa8fb4
pmargreff <pmargreff@gmail.com>
2019-05-10 23:34:14 -0300

    Remove unused functions

--- lib/paint_easy.ex
+++ lib/paint_easy.ex
@@ -1,24 +1,3 @@
 defmodule PaintEasy do
-  alias PaintEasy.{Image, Pixel, Writer}
-
-  def paint_pixel(%Image{} = image, %Pixel{color: color, x: x, y: y}) do
-    new_matrix = generate_new_matrix(image.pixels, color, x, y)
-    Map.put(image, :pixels, new_matrix)
-  end
-
-  def write(%Image{} = image, filepath \\ DateTime.utc_now() |> DateTime.to_string()) do
-    Writer.create_file(filepath, image)
-  end
-
-  defp generate_new_matrix(pixels, color, x, y) do
-    new_line = generate_new_line(pixels, color, x, y)
-    List.replace_at(pixels, y, new_line)
-  end
-
-  defp generate_new_line(pixels_matrix, color, x, y) do
-    Enum.fetch(pixels_matrix, y)
-    |> Tuple.to_list()
-    |> List.last()
-    |> List.replace_at(x, color)
-  end
+  @moduledoc false
 end

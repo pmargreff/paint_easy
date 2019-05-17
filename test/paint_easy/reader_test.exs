1f09863ea4457c7781ff22cc8bd35014894b32bb
pmargreff <pmargreff@gmail.com>
2019-05-17 09:19:44 -0300

    feature(read): allow to read pgm images

--- test/paint_easy/reader_test.exs
+++ test/paint_easy/reader_test.exs
@@ -27,5 +27,24 @@ defmodule PaintEasy.ReaderTest do
                %Pixel{color: 0, x: 2, y: 1}
              ]
     end
+
+    @tag dev: true
+    test "read pgm image file on ascii format" do
+      fixture_file = File.cwd!() <> "/test/fixtures/ascii_image.pgm"
+      assert {:ok, image} = Reader.read_file(fixture_file)
+      assert image.code == "P2"
+      assert image.width == 3
+      assert image.height == 2
+
+      assert length(image.pixels) == 6
+      assert image.pixels == [
+               %Pixel{color: 255, x: 0, y: 0},
+               %Pixel{color: 0, x: 1, y: 0},
+               %Pixel{color: 255, x: 2, y: 0},
+               %Pixel{color: 0, x: 0, y: 1},
+               %Pixel{color: 255, x: 1, y: 1},
+               %Pixel{color: 0, x: 2, y: 1}
+             ]
+    end
   end
 end

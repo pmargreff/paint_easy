cbb2e443d27d7f2b3d9891cb846bff3e80bc951d
pmargreff <pmargreff@gmail.com>
2019-05-18 18:28:21 -0300

    fix(filter): assert lazy edition

--- lib/paint_easy/editor.ex
+++ lib/paint_easy/editor.ex
@@ -5,7 +5,6 @@ defmodule PaintEasy.Editor do
 
   def edit(%{pixels: pixels} = image, filter) do
     new_pixels = Stream.map(pixels, fn pixel -> filter.(pixel, image) end)
-    |> Enum.to_list()
 
     Map.put(image, :pixels, new_pixels)
   end
